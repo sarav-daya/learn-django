ba6d668c7a9b0e1970a1c15617907b4092cd0f6c
Saravanan Dayalan <dsaravanan.che@gmail.com>
2021-08-23 16:40:00 +0530

    added styles

--- src/projects/forms.py
+++ src/projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -14,3 +15,14 @@ class ProjectForm(ModelForm):
             "source_link",
             "tags",
         ]
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        # self.fields['title'].widget.attrs.update({'class' : 'input', 'placeholder' : 'Project title goes here.'})
+        # self.fields['description'].widget.attrs.update({'class' : 'input', 'placeholder' : 'Project description goes here.'})
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input"})
