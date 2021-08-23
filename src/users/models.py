50eb2e1ad150e79858e8ee85bb168a321e6b1c82
Saravanan Dayalan <dsaravanan.che@gmail.com>
2021-08-23 21:21:46 +0530

    User profile built

--- src/users/models.py
+++ src/users/models.py
@@ -13,9 +13,12 @@ class Profile(models.Model):
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(
-        blank=True, null=True, upload_to="profiles/", default="profiles/user-default.png"
+        blank=True,
+        null=True,
+        upload_to="profiles/",
+        default="profiles/user-default.png",
     )
-
+    location = models.CharField(max_length=200, blank=True, null=True)  
     social_github = models.CharField(max_length=200, blank=True, null=True)
     social_twitter = models.CharField(max_length=200, blank=True, null=True)
     social_linkedin = models.CharField(max_length=200, blank=True, null=True)
@@ -29,3 +32,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return str(self.name)
--- src/users/views.py
+++ src/users/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from users.models import Profile
 
 # Create your views here.
 def profiles(request):
-    return render(request, "users/profiles.html")
+    profiles = Profile.objects.all()
+    context = {"profiles": profiles}
+    return render(request, "users/profiles.html", context)
+
+
+def user_profile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    top_skills = profile.skill_set.exclude(description__exact="")
+    other_skills = profile.skill_set.filter(description__exact="")
+    context = {
+        "profile": profile,
+        "top_skills": top_skills,
+        "other_skills": other_skills,
+    }
+    return render(request, "users/user-profile.html", context)
